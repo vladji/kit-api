1f28a11e504d7838f1568921ef5e674062111302
vladji <vladjislav@gmail.com>
2025-07-26 22:03:28 +0700

    perf: check pass, create hash

---
+++ src/app/crypto/createHash.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+export const createHash = async (password) => {
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(password, salt);
+  // console.log("hash", hash);
+  // return hash;
+};
+
+const pass = "88";
+createHash(pass);
