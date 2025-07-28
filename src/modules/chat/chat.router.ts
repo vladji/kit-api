85f5f70bc4c982351081037b31487fba135f5a0b
vladji <vladjislav@gmail.com>
2025-07-29 02:00:00 +0700

    perf: chat router, controller

--- src/modules/chat/chat.router.ts
+++ src/modules/chat/chat.router.ts
@@ -1,18 +1,11 @@
 import express from "express";
 import { checkApiKey } from "../../shared/middlewares/checkApiKey";
-import { getAllSupportChats, getMemberAllChats } from "./controllers/chat";
 import { getMessages } from "./controllers/message";
-import { requireAdmin } from "../../shared/middlewares/requireRole";
+import { getMemberChats } from "./controllers/chat";
 
 const router = express.Router();
 
-router.get(
-  "/chat/admin/all-support",
-  checkApiKey,
-  requireAdmin,
-  getAllSupportChats
-);
-router.get("/chat/member/all-chats", checkApiKey, getMemberAllChats);
+router.get("/chats/member", checkApiKey, getMemberChats);
 router.get("/chat/messages", checkApiKey, getMessages);
 
 export default router;
--- src/modules/chat/controllers/chat.ts
+++ src/modules/chat/controllers/chat.ts
@@ -3,21 +3,30 @@ import { ChatModel } from "../model/chat";
 import { errorHandler } from "../../../shared/middlewares/errorHandler";
 import { pagination } from "../../../shared/utils/pagination";
 
-export const getMemberAllChats = async (req: Request, res: Response) => {
+export const getMemberChats = async (req: Request, res: Response) => {
   try {
     const {
       page, limit, skip
     } = pagination(req);
 
-    const memberId = req.query.memberId;
+    const memberId = req.query.memberId as string;
+    const support = req.query.support === "true";
+
+    const filter: any = {
+      "members.id": memberId,
+    };
+
+    if (req.query.support !== undefined) {
+      filter.support = support;
+    }
 
     const [chats, total] = await Promise.all([
       ChatModel
-        .find({ "members.id": memberId })
+        .find(filter)
         .sort({ updatedAt: -1 })
         .skip(skip)
         .limit(limit),
-      ChatModel.countDocuments({ "members.id": memberId }),
+      ChatModel.countDocuments(filter),
     ]);
 
     res.status(200).json({
@@ -32,30 +41,5 @@ export const getMemberAllChats = async (req: Request, res: Response) => {
   }
 };
 
-export const getAllSupportChats = async (req: Request, res: Response) => {
-  try {
-    const {
-      page, limit, skip
-    } = pagination(req);
 
-    const [chats, total] = await Promise.all([
-      ChatModel
-        .find({ support: true })
-        .sort({ updatedAt: -1 })
-        .skip(skip)
-        .limit(limit),
-      ChatModel.countDocuments({ support: true }),
-    ]);
-
-    res.status(200).json({
-      total,
-      page,
-      totalPages: Math.ceil(total / limit),
-      chats,
-    });
-    return;
-  } catch (err) {
-    return errorHandler(err, req, res);
-  }
-};
 
